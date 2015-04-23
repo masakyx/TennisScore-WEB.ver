a175ca4742dda14ef64eb2bfbc36cd59dfda2563
masakyx <weavermasaki@gmail.com>
2015-04-23 20:49:08 +0900

    ちょっと修正

--- public/javascripts/stage4.js
+++ public/javascripts/stage4.js
@@ -21,7 +21,7 @@ jQuery(function($){
       }else{
         var time = new Date();
         var year = time.getFullYear(),
-            month = time.getMonth(),
+            month = time.getMonth() + 1,
             day = time.getDate(),
             ji = time.getHours(),
             hun = time.getMinutes(),
